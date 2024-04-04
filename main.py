fc38170dcf381638728c7e6f16d845ef1390e09e
Daniil Roman <danroman17397@gmail.com>
2024-04-04 23:56:13 +0200

    add batching

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from src.logic import stock_finder
 from src.logic.data import data
 
 if __name__ == '__main__':
-    try:
-        stock_finder.run()
-    except data.SkipException as e:
-        logging.error(e)
+    for i in range(5):
+        try:
+            stock_finder.run()
+        except data.SkipException as e:
+            logging.error(e)
