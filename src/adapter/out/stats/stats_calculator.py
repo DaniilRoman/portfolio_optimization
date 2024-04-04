ea3861f04b0a368161cce92c5618f39b4bca39e3
Daniil Roman <danroman17397@gmail.com>
2024-04-04 21:49:47 +0200

    Add stats calculation for negative industry

--- src/adapter/out/stats/stats_calculator.py
+++ src/adapter/out/stats/stats_calculator.py
@@ -4,5 +4,7 @@ import configuration
 
 def calculate(analyses_result: StockData):
     if analyses_result.is_stock_growing and analyses_result.profitability_data.is_profitable():
-        appScriptEndpoint = f'https://script.google.com/macros/s/{configuration.APP_SCRIPT_ID}/exec?value="{analyses_result.industry}"&sheet="industry"'
-        requests.get(appScriptEndpoint)
+        increment_counter_endpoint = f'https://script.google.com/macros/s/{configuration.APP_SCRIPT_ID}/exec?value="{analyses_result.industry}"&sheet="industry"'
+    else:
+        increment_counter_endpoint = f'https://script.google.com/macros/s/{configuration.APP_SCRIPT_ID}/exec?value="{analyses_result.industry}"&sheet="negative_industry"'
+    requests.get(increment_counter_endpoint)
