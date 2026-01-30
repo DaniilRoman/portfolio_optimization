5eb7088320c02339a428cc81eb51937c52c8ad06
Daniil Roman <danroman17397@gmail.com>
2026-01-30 08:22:30 +0100

    update

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import logging
 
 from src.logic import stock_finder
 from src.logic.data import data
+from stock_names import etf_list
 
 if __name__ == '__main__':
-    for i in range(3):
+    for i in range(len(etf_list)):
         try:
             stock_finder.run()
         except data.SkipException as e:
